e481f65ec6a6aa6ab64caa275464a14770afa5c7
Mohamed <97177001+Mohamed110011@users.noreply.github.com>
2023-12-10 00:15:49 +0100

    Add files via upload
    prosit 1

---
+++ ZooManagement.java
@@ -0,0 +1,11 @@
+public class ZooManagement {
+
+    int nbrCages=20;
+    String zooName="my zoo" ;
+
+    public static void main(String[] args) {
+        //Instruction 1
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+}
+}
